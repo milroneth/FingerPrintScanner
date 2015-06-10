c20dcfe47f71780e199a801e94b363e5930a85d7
MobiusTesseract <mobiustesseract+github@gmail.com>
2015-06-10 04:30:23 +0300

    Code Upload

---
+++ src/Admins.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+
+public class Admins extends User{
+	
+	
+	public Admins(String name,String surname,Date date,int code){
+		super(name,surname,date,code);
+	}
+
+}
---
+++ src/Clients.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+
+public class Clients extends User{
+	
+	public Clients(String name,String surname,Date date,int code){
+		super(name,surname,date,code);
+	}
+	
+}
