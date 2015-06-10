625883508773d7a5a0b7298643a43df76e978848
MobiusTesseract <mobiustesseract+github@gmail.com>
2015-06-11 01:19:34 +0300

    Wrote Connect/Disconnect Methods

--- src/gr/uom/softeng2015/team28/SerialConnectFrame.java
+++ src/gr/uom/softeng2015/team28/SerialConnectFrame.java
@@ -17,8 +17,12 @@ import javax.swing.JTextArea;
 import jssc.SerialPort;
 import jssc.SerialPortException;
 import jssc.SerialPortList;
+import jssc.SerialPortTimeoutException;
 
 public class SerialConnectFrame extends JFrame {
+
+	private String selectedPort = "NULL";
+	private SerialPort arduinoPort = new SerialPort(selectedPort);
 
 	private JPanel portPanel;
 	private JLabel portSelectPrompt;
@@ -86,6 +90,60 @@ public class SerialConnectFrame extends JFrame {
 		}
 		portList.setSelectedIndex(portList.getItemCount() - 1);
 	}                                            
+
+	private void connectToArduino() {
+
+		byte sensorFound = -1;
+		arduinoPort = new SerialPort(selectedPort);
+
+		try {
+			arduinoPort.openPort();	//Open serial port
+			arduinoPort.setParams(SerialPort.BAUDRATE_9600, 
+					SerialPort.DATABITS_8,
+					SerialPort.STOPBITS_1,
+					SerialPort.PARITY_NONE);	//Set params. Also you can set params by this string: serialPort.setParams(9600, 8, 1, 0);
+			sensorFound = arduinoPort.readBytes(1, 10000)[0];
+		} catch (SerialPortException serialEx) {
+			System.out.println(serialEx);
+			terminalText.append("Error opening port.\n");
+		} catch (SerialPortTimeoutException timeoutEx) {
+			System.out.println(timeoutEx);
+			terminalText.append("Connection timeout. Is the Arduino connected to " + selectedPort + " and running properly?\n");
+		}
+
+		terminalText.append("Connected to Arduino!\n");
+		if(sensorFound == '0') {
+			terminalText.append("No fingerprint sensor found!\n");
+		}
+		else if(sensorFound == '1') {
+			terminalText.append("Connected to Fingerprint sensor!\n");
+		}
+		else {
+			terminalText.append("Uknown error!\n");
+		}
+
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			public void run() {
+				try {
+					arduinoPort.closePort();
+					System.out.println("Shutdown hook: Disconnected.\n");
+				} catch (SerialPortException serialEx) {
+					System.out.println("Shutdown hook: Port not opened.\n");
+				}
+			}
+		});
+	}
+
+	private void disconnectFromArduino() {
+
+		try {
+			arduinoPort.writeBytes("1003".getBytes());	//Test writing data to port
+			arduinoPort.closePort();
+			terminalText.append("Disconnected.\n");
+		} catch (SerialPortException serialEx) {
+			terminalText.append("Port not opened.\n");
+		}
+	}
 
 	class ButtonListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
